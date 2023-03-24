ee8062d7dbc2ec7fcc5360ff8da4c2749b628e36
YevaBrand <yevaalessa@gmail.com>
2023-03-24 01:09:23 +0000

    update

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,7 +7,6 @@
  */
 void print_diagonal(int n)
 {
-	int i, j;
 
 	if (n <= 0)
 	{
@@ -15,13 +14,17 @@ void print_diagonal(int n)
 	}
 	else
 	{
-		for (i = 1; i <= n; i++)
+		int i, j;
+
+		for (i = 0; i < n; i++)
 		{
-			for (j = 1; j < co; j++)
+			for (j = 0; j < n; j++)
 			{
-				_putchar(' ');
+				if (j == i)
+					_putchar('\\');
+				else if (j < i)
+					_putchar(' ');
 			}
-			_putchar('\\');
 			_putchar('\n');
 		}
 	}
