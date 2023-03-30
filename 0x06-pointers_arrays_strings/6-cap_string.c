86f07f9750e905c68f8a4d77d514e39a014099af
YevaBrand <yevaalessa@gmail.com>
2023-03-30 21:48:07 +0000

    task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -12,21 +12,21 @@ char *cap_string(char *str)
 	int a[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
 	i = 0;
-	while (*(s + i) != '\0')
+	while (*(str + i) != '\0')
 	{
-		if (*(s + i) >= 'a' && *(s + i) <= 'z')
+		if (*(str + i) >= 'a' && *(str + i) <= 'z')
 		{
 			if (i == 0)
 			{
-				*(s + i) = *(s + i) - 32;
+				*(str + i) = *(str + i) - 32;
 			}
 			else
 			{
 				for (j = 0; j <= 12; j++)
 				{
-					if (a[j] == *(s + i - 1))
+					if (a[j] == *(str + i - 1))
 					{
-						*(s + i) = *(s + i) - 32;
+						*(str + i) = *(str + i) - 32;
 					}
 				}
 			}
